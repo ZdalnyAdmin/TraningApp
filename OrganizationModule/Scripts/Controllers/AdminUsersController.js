15357e6192e1405d661a78df35573790e056b9cf
ZdalnyAdmin <michal.blaszczak@zdalnyadmin.com.pl>
2015-04-22 22:51:52 +0200

    User view , communcation, angular contorller, asp models and controllers

---
+++ OrganizationModule/Scripts/Controllers/AdminUsersController.js
@@ -0,0 +1,16 @@
+﻿function adminUsersController($scope, $http) {
+    $scope.loading = true;
+    $scope.addMode = false;
+
+    $scope.status = ['aktywny','zablokowany'];
+
+    //Used to display the data 
+    $http.get('/api/Person').success(function (data) {
+        $scope.Persons = data;
+        $scope.loading = false;
+    })
+    .error(function () {
+        $scope.error = "An Error has occured while loading posts!";
+        $scope.loading = false;
+    });
+}
