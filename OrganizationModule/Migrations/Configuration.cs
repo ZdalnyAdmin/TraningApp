db9f0b4f0839c1bd294dd30e271a4d90d53fcd12
ZdalnyAdmin <michal.blaszczak@zdalnyadmin.com.pl>
2015-04-24 13:11:56 +0200

    Changed schema for indentity

--- OrganizationModule/Migrations/Configuration.cs
+++ OrganizationModule/Migrations/Configuration.cs
@@ -10,6 +10,7 @@ namespace OrganizationModule.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(OrganizationModule.Models.ApplicationDbContext context)
