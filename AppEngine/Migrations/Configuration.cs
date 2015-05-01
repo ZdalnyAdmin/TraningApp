d34b27c9ef647977031a7f58561b81f3b67ef2b1
ZdalnyAdmin <michal.blaszczak@zdalnyadmin.com.pl>
2015-05-01 22:44:53 +0200

    user seed.

--- AppEngine/Migrations/Configuration.cs
+++ AppEngine/Migrations/Configuration.cs
@@ -17,6 +17,18 @@
         protected override void Seed(AppEngine.Models.DataContext.EFContext context)
         {
             context.Organizations.AddOrUpdate(new Organization() { Name = "My First Organization", CreateDate = DateTime.Now });
+            context.Users.AddOrUpdate(new Person() { Id = "9154c72e-a195-4a70-8de2-61927df9bd9d",
+                                                     Email = "email@mail.pl",
+                                                     PasswordHash = "AGE09SnBYFYs5KB409zx1/T5mIf6Z1gef2QD6mMyr8DIYtpyMtT9vMYWP/Mpyj7JgQ==", // Admin1!
+                                                     SecurityStamp =  "fd2e32d1-5280-48d4-8963-57f48b35dbd7",
+                                                     TwoFactorEnabled = true,
+                                                     UserName = "admin",
+                                                     Profile = Models.DataBusiness.ProfileEnum.Superuser,
+                                                     Status = Models.DataBusiness.StatusEnum.Active,
+                                                     RegistrationDate = DateTime.Now,
+                                                     InvitationDate = DateTime.Now,
+                                                     InviterID = "9154c72e-a195-4a70-8de2-61927df9bd9d"
+                                                    });
         }
     }
 }
