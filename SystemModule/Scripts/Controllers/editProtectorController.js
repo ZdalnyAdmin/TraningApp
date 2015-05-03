9124209bcec19436bc6af08e10985dee6521a53a
ZdalnyAdmin <michal.blaszczak@zdalnyadmin.com.pl>
2015-05-03 20:15:30 +0200

    Application configuration for System

---
+++ SystemModule/Scripts/Controllers/editProtectorController.js
@@ -0,0 +1,9 @@
+﻿function editProtectorController($scope, $http, $modal) {
+    $scope.loading = true;
+    $scope.list = [];
+    $scope.editable = {};
+
+
+}
+
+editProtectorController.$inject = ['$scope', '$http', '$modal'];
