e0b927aa734fea306d8d63ab509f0d8c07543a5d
ZdalnyAdmin <michal.blaszczak@zdalnyadmin.com.pl>
2015-05-07 18:18:22 +0200

    Delete previous file.

---
+++ AppEngine/Models/ViewModels/UploadViewModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppEngine.Models.ViewModels.Upload
+{
+    public class DeleteViewModel
+    {
+        [Required]
+        public string FileName { get; set; }
+    }
+}
--- SystemModule/Controllers/RedirectController.cs
+++ SystemModule/Controllers/RedirectController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace OrganizationModule.Controllers
+namespace SystemModule.Controllers
 {
     public class RedirectController : Controller
     {
