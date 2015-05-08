52da27b364d70afd3e7c68775b9730b012b79b3d
ZdalnyAdmin <michal.blaszczak@zdalnyadmin.com.pl>
2015-05-08 22:35:28 +0200

    Merge Fix.

--- SystemModule/Scripts/Controllers/LoginController.js
+++ SystemModule/Scripts/Controllers/LoginController.js
@@ -1,4 +1,4 @@
-﻿var LoginController = function ($scope, $routeParams, $location, UserFactory) {
+﻿var LoginController = function ($scope, $routeParams, $location, UserFactory, UtilitiesFactory, $rootScope) {
     $scope.loginForm = {
         emailAddress: '',
         password: '',
@@ -7,21 +7,28 @@
     };
 
     $scope.login = function () {
+        $scope.processing = true;
         UtilitiesFactory.showSpinner();
         var result = UserFactory.login($scope.loginForm.emailAddress, $scope.loginForm.password);
         result.then(function (result) {
             UtilitiesFactory.hideSpinner();
+            $scope.processing = false;
+            $rootScope.$broadcast('userChanged');
+
             if (result.success) {
                 if ($scope.loginForm.returnUrl !== undefined) {
                     $location.path($scope.loginForm.returnUrl).search('');
                 } else {
                     $location.path('/').search('');
                 }
+
             } else {
                 $scope.loginForm.loginFailure = true;
+                $scope.processing = false;
             }
+
         });
     }
 };
 
-LoginController.$inject = ['$scope', '$routeParams', '$location', 'UserFactory', 'UtilitiesFactory'];
+LoginController.$inject = ['$scope', '$routeParams', '$location', 'UserFactory', 'UtilitiesFactory', '$rootScope'];
