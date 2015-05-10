225be6fadcf5d9f987712860f68d5aa6c839184a
ZdalnyAdmin <michal.blaszczak@zdalnyadmin.com.pl>
2015-05-10 19:21:24 +0200

    Load seed

--- AppEngine/Models/DataContext/EFContext.cs
+++ AppEngine/Models/DataContext/EFContext.cs
@@ -7,15 +7,29 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using AppEngine.Migrations;
 
 namespace AppEngine.Models.DataContext
 {
     public class EFContext : IdentityDbContext<Person> 
     {
+        public static bool firsLog = true;
+
+
         public EFContext()
             : base("name=DefaultConnection", throwIfV1Schema: false)
         {
             base.Configuration.ProxyCreationEnabled = false;
+            if (!Database.Exists())
+            {
+                Database.Initialize(true);
+            }  
+
+            if(firsLog)
+            {
+                firsLog = false;
+                new Configuration().CallSeed(this);
+            }
         }
 
         public static EFContext Create()
@@ -37,18 +51,15 @@ namespace AppEngine.Models.DataContext
         public DbSet<TrainingQuestion> TrainingQuestons { get; set; }
         public DbSet<Trainings2Organizations> TrainingsInOrganizations { get; set; }
 
-        protected override void OnModelCreating(System.Data.Entity.DbModelBuilder modelBuilder)
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
-
             modelBuilder.Entity<IdentityUser>().ToTable("Person", "dbo");
             modelBuilder.Entity<Person>().ToTable("Person", "dbo");
             modelBuilder.Entity<IdentityRole>().ToTable("Roles", "dbo");
             modelBuilder.Entity<IdentityUserRole>().ToTable("UserRoles", "dbo");
             modelBuilder.Entity<IdentityUserClaim>().ToTable("UserClaims", "dbo");
             modelBuilder.Entity<IdentityUserLogin>().ToTable("UserLogins", "dbo");
-
         }
 
     }
