83429bf0b30f9a92b3f634a4cde270a98387bbdb
ZdalnyAdmin <michal.blaszczak@zdalnyadmin.com.pl>
2015-05-10 22:31:53 +0200

    Customized menu.

--- OrganizationModule/Controllers/MenuController.cs
+++ OrganizationModule/Controllers/MenuController.cs
@@ -18,39 +18,37 @@ namespace OrganizationModule.Controllers
         [ChildActionOnly]
         public ActionResult UserMenu()
         {
+            fillData();
             return View();
         }
+
         [ChildActionOnly]
         public ActionResult CreatorMenu()
         {
+            fillData();
             return View();
         }
+
         [ChildActionOnly]
         public ActionResult ManagerMenu()
         {
+            fillData();
+
             return View();
         }
+
         [ChildActionOnly]
         public ActionResult AdminMenu()
         {
+            fillData();
+
             return View();
         }
+
         [ChildActionOnly]
         public ActionResult KeeperMenu()
         {
-            var loggedPerson = Person.GetLoggedPerson(User);
-            var trainings = new List<Training>();
-
-            trainings = _db.Trainings
-                            .Join(_db.TrainingResults
-                                     .Where(y => y.PersonID == loggedPerson.Id && y.EndDate == null),
-                                  x => x.TrainingID,
-                                  y => y.TrainingID,
-                                  (x, y) => x)
-                            .ToList();
-
-            ViewBag.LoggedUser = loggedPerson;
-            ViewBag.Trainings = trainings;
+            fillData();
 
             return View();
         }
@@ -58,11 +56,35 @@ namespace OrganizationModule.Controllers
         public ActionResult Index()
         {
             var loggedPerson = Person.GetLoggedPerson(User);
+            ViewBag.LoggedUser = loggedPerson;
+
+            return View();
+        }
+
+        #region Private Methods
+        private List<Training> getTrainings(string personID)
+        {
             var trainings = new List<Training>();
+
+            trainings = _db.Trainings
+                            .Join(_db.TrainingResults
+                                     .Where(y => y.PersonID == personID && y.EndDate == null),
+                                  x => x.TrainingID,
+                                  y => y.TrainingID,
+                                  (x, y) => x)
+                            .ToList();
+
+            return trainings;
+        }
+
+        private void fillData()
+        {
+            var loggedPerson = Person.GetLoggedPerson(User);
+            var trainings = getTrainings(loggedPerson.Id);
 
             ViewBag.LoggedUser = loggedPerson;
             ViewBag.Trainings = trainings;
-            return View();
         }
+        #endregion
     }
 }
