ec8357d2a418e11c9a73935cfaf97db6a75d0009
ZdalnyAdmin <michal.blaszczak@zdalnyadmin.com.pl>
2015-05-13 19:58:14 +0200

    Show/hide details.

--- OrganizationModule/Scripts/Controllers/trainingListController.js
+++ OrganizationModule/Scripts/Controllers/trainingListController.js
@@ -1,6 +1,38 @@
 ﻿function trainingListController($scope, $http, $rootScope, $modal) {
     $scope.display = 'All';
 
+    var trainings = angular.element.find('.training');
+
+    angular.forEach(trainings, function (t) {
+        var training = $(t);
+
+        var desc = training.find('.description');
+        var content = desc.html();
+
+        if (content.length > 600) {
+            var shortDesc = content.substring(0, 599) + '...';
+            training.find('.hide-details').hide();
+            desc.html(shortDesc);
+
+            training.find('.details').click(function () {
+                training.find('.hide-details').show();
+                training.find('.details').hide();
+
+                desc.html(content);
+            });
+
+            training.find('.hide-details').click(function () {
+                training.find('.hide-details').hide();
+                training.find('.details').show();
+
+                desc.html(shortDesc);
+            });
+        } else {
+            training.find('.details').hide();
+            training.find('.hide-details').hide();
+        }
+    });
+
     $scope.changeDisplayedTrainings = function (mode) {
         $scope.display = mode;
     };
