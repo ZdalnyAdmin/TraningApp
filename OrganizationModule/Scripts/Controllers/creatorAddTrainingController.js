12362fe301a481b731ddc79482d1863e893d3246
ZdalnyAdmin <michal.blaszczak@zdalnyadmin.com.pl>
2015-06-01 17:08:33 +0200

    marge

--- OrganizationModule/Scripts/Controllers/creatorAddTrainingController.js
+++ OrganizationModule/Scripts/Controllers/creatorAddTrainingController.js
@@ -34,15 +34,15 @@
                 deleteFile($scope.viewModel.Current.TrainingResources);
             }
 
-            checkImageArtibutesAndUpload(file, 350, 250, 250, false);
+            var statusBar = $('.statusBar').eq(0);
+
+            checkImageArtibutesAndUpload(file, 350, 250, 250, false, statusBar);
         });
     }
 
     $scope.loadIcon = function (item) {
         $scope.$apply(function (scope) {
             var file = $element[0].getElementsByClassName('upload-mark')[0].files[0];
-
-
 
             if (!checkExtension(file)) {
                 $scope.markMessage = 'Plik może być w formacie JPG, JPEG, PNG, GIF lub BMP.';
@@ -53,11 +53,13 @@
                 deleteFile($scope.viewModel.Current.PassResources);
             }
 
-            checkImageArtibutesAndUpload(file, 150, 150, 150, true);
+            var statusBar = $('.statusBarMark').eq(0);
+
+            checkImageArtibutesAndUpload(file, 150, 150, 150, true, statusBar);
         });
     }
 
-    function checkImageArtibutesAndUpload(file, maxSize, maxWidth, maxHeight, marks) {
+    function checkImageArtibutesAndUpload(file, maxSize, maxWidth, maxHeight, marks, statusBar) {
         $scope.fileName = file.name;
         var size = ~~(file.size / 1024);
 
@@ -82,13 +84,14 @@
 
             window.URL.revokeObjectURL(img.src);
 
+
             if (width == maxWidth && height == maxHeight) {
                 var fd = new FormData();
                 fd.append('file', file);
                 if (marks) {
-                    sendFileToServer(fd, new createMarkStatusbar($element[0].getElementsByClassName('statusBarMark')), marks);
+                    sendFileToServer(fd, new createMarkStatusbar(statusBar || $element[0].getElementsByClassName('statusBarMark')), marks);
                 } else {
-                    sendFileToServer(fd, new createStatusbar($element[0].getElementsByClassName('statusBar')), marks);
+                    sendFileToServer(fd, new createStatusbar(statusBar || $element[0].getElementsByClassName('statusBar')), marks);
                 }
             }
             else {
@@ -182,7 +185,7 @@
 
                     if (marks) {
                         $scope.markMessage = 'Plik nie mógł zostać wczytany - spróbuj ponownie później.';
-                    }
+            }
                     else {
                         $scope.imageMessage = 'Plik nie mógł zostać wczytany - spróbuj ponownie później.';
                     }
@@ -216,7 +219,7 @@
 
         this.setProgress = function (progress) {
             var progressBarWidth = progress * this.progressBar.width() / 100;
-            this.progressBar.find('div').animate({ width: progressBarWidth }, 10).html(progress + "% ");
+            this.progressBar.find('div').animate({ width: progressBarWidth }, 10);
 
             $scope.$apply();
         }
