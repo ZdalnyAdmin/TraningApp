e9d4d31b78edc670a2ba685b7d20a110db8f44c2
ZdalnyAdmin <michal.blaszczak@zdalnyadmin.com.pl>
2015-06-12 19:52:26 +0200

    Logging functionality.

---
+++ AppEngine/Helpers/Log.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+
+namespace AppEngine.Helpers
+{
+    public class Log : HttpServerUtilityBase
+    {
+        private enum LogType
+        {
+            INFO = 0,
+            WARNING = 1,
+            ERROR = 2
+        }
+
+        static readonly TextWriter logTW;
+
+        static Log()
+        {
+            var logPath = HostingEnvironment.MapPath(Path.Combine("~/Log/", "log.txt"));
+
+            if (!Directory.Exists(HostingEnvironment.MapPath("~/Log/")))
+            {
+                Directory.CreateDirectory(HostingEnvironment.MapPath("~/Log/"));
+            }
+
+            logTW = TextWriter.Synchronized(File.AppendText(logPath)); 
+        }
+
+        public static void Info(string message)
+        {
+            AddToLog(message, LogType.INFO);
+        }
+
+        public static void Warning(string message)
+        {
+            AddToLog(message, LogType.WARNING);
+        }
+
+        public static void Error(string message)
+        {
+            AddToLog(message, LogType.ERROR);
+        }
+
+        private static void AddToLog(string message, LogType type)
+        {
+            var logDate = DateTime.Now;
+            var logType = string.Empty;
+
+            switch (type)
+            {
+                case LogType.ERROR:
+                    logType = "ERROR";
+                    break;
+
+                case LogType.WARNING:
+                    logType = "WARNING";
+                    break;
+
+                case LogType.INFO:
+                default:
+                    logType = "INFO";
+                    break;
+            }
+
+            var logMessage = string.Format("{0} {1}: {2}",
+                                            logDate.ToString("dd/MM/yyyy HH:mm:ss"),
+                                            logType,
+                                            message);
+
+            logTW.WriteLine(logMessage);
+            logTW.Flush();
+        }
+    }
+}
--- AppEngine/Helpers/Mail.cs
+++ AppEngine/Helpers/Mail.cs
@@ -38,10 +38,13 @@ namespace AppEngine.Helpers
                     client.Credentials = getNetworkCredential(account);
                     client.Send(message);
                 }
+
+                Log.Info(string.Format("Wiadomość została wysłana do {0}", message.To.ToString()));
             }
             catch (Exception ex)
             {
-                // TODO 
+                Log.Error(string.Format("Wystąpił błąd podczas wysyłania wiadomości do {0}", message.To.ToString()));
+                Log.Error(ex.StackTrace);
             }
         }
 
