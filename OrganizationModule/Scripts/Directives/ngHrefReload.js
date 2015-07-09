e516b272e3c4c8868a828c925a53c8cd89a68f06
ZdalnyAdmin <michal.blaszczak@zdalnyadmin.com.pl>
2015-07-09 16:37:10 +0200

    Download file Reload training list.

---
+++ OrganizationModule/Scripts/Directives/ngHrefReload.js
@@ -0,0 +1,18 @@
+﻿window.App
+.directive('ngHrefReload', ['$location', '$route',
+        function ($location, $route) {
+            return function (scope, element, attrs) {
+                scope.$watch('ngHrefReload', function () {
+                    if (attrs.ngHrefReload) {
+                        element.attr('href', attrs.ngHrefReload);
+                        element.bind('click', function (event) {
+                            scope.$apply(function () {
+                                if ($location.path() == attrs.ngHrefReload) {
+                                    $route.reload();
+                                }
+                            });
+                        });
+                    }
+                });
+            }
+        }]);
